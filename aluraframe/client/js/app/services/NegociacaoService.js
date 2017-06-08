ef77dee9f1a4a00bd0927c6834f084722fc21030
cabecacl <cabeca.cl@gmail.com>
2017-06-08 05:26:28 -0300

    Continuação do projeto de aula

--- aluraframe/client/js/app/services/NegociacaoService.js
+++ aluraframe/client/js/app/services/NegociacaoService.js
@@ -59,19 +59,67 @@ class NegociacaoService {
                 this.obterNegociacoesDaSemanaAnterior(),
                 this.obterNegociacoesDaSemanaRetrasada()]
             ).then(negociacoes => {
-                
+
                 let listaNegociacoes = [];
                 negociacoes
                     .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
                     .forEach(negociacao => listaNegociacoes.push(negociacao));
 
                 resolve(listaNegociacoes);
-                
+
             })
                 .catch(error => reject(error));
         });
     }
+cadastra(negociacao) {
 
+      return ConnectionFactory
+         .getConnection()
+         .then(connection => new NegociacaoDao(connection))
+         .then(dao => dao.adiciona(negociacao))
+         .then(() => 'Negociação cadastrada com sucesso')
+         .catch(erro => {
+            console.log(erro);
+            throw new Error('Não foi possível adicionar a negociação')
+         });
+ }
 
+    lista() {
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .catch(erro => {
+                console.log(error);
+                throw new Error('Não foi possivel obter as negociações')
+            })
+    }
+
+    apaga() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(() => 'Negociações apagadas com sucesso')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível apagar as negociações')
+            })
+    }
+
+    importa(listaAtual) {
+
+       return this.obterNegociacoes()
+           .then(negociacoes =>
+               negociacoes.filter(negociacao =>
+                   !listaAtual.some(negociacaoExistente =>
+                       JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
+           )
+           .catch(erro => {
+               console.log(erro);
+               throw new Error("Não foi possível importar as negociações");
+           });
+   }
 
 }
